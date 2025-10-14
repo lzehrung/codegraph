fabcf23e2910c1c1e45ecca255f6fbb1b6e4c2ff
Luke Zehrung <lzehrung@gmail.com>
2025-10-14 09:47:07 -0400

    add more python implementation

---
+++ test_python/utils.py
@@ -0,0 +1,16 @@
+def helper_function():
+    """A helper function."""
+    return "Hello from utils"
+
+class UtilityClass:
+    """A utility class."""
+    
+    def __init__(self):
+        self.value = 42
+    
+    def get_value(self):
+        return self.value
+
+# Export specific items
+__all__ = ["helper_function", "UtilityClass"]
+
