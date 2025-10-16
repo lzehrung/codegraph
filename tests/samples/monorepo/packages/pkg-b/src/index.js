98112ef73b31fcf9308ab57a7741c51c3fac02a0
Luke Zehrung <me@lukezehrung.com>
2025-10-15 23:22:07 -0400

    cross package

--- tests/samples/monorepo/packages/pkg-b/src/index.js
+++ tests/samples/monorepo/packages/pkg-b/src/index.js
@@ -1,4 +1,5 @@
 import { aHelper, AClass, A_CONST } from '@acme/pkg-a';
+import * as a from '@acme/pkg-a';
 
 export function bUseA() {
   return aHelper() + A_CONST;
@@ -7,5 +8,13 @@ export function bUseA() {
 export function bMakeA() {
   return new AClass(2);
 }
+
+// Namespace import usages
+const nsVal = a.aHelper();
+const nsObj = new a.AClass(5);
+
+// CommonJS require destructuring (for parsing/graph tests)
+const { aHelper: requireAlias } = require('@acme/pkg-a');
+const reqVal = requireAlias();
 
 
