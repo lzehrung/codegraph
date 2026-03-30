f729db4384de57cb23d1daa342bb9bfa6c1a0d1d
Luke Zehrung <me@lukezehrung.com>
2026-03-30 16:17:24 -0400

    angularjs support

---
+++ tests/samples/frameworks/angularjs/graph/user.controller.js
@@ -0,0 +1,10 @@
+angular.module("admin").controller("UserCtrl", [
+  "$scope",
+  "$state",
+  "userService",
+  function UserCtrl($scope, $state, userService) {
+    $scope.refresh = function refresh() {
+      return userService.load();
+    };
+  },
+]);
